f5669c8d60cd32b15c2f4b18243dc97eb423243b
anthonyx159 <60250281+anthonyx159@users.noreply.github.com>
2020-12-28 02:50:06 -0500

    Update model_loader.py

--- scripts/model_loader.py
+++ scripts/model_loader.py
@@ -8,7 +8,7 @@ from keras.models import load_model
 
 def cargarModelo():
 
-    FILENAME_MODEL_TO_LOAD = "covid19_model_full.h5"
+    FILENAME_MODEL_TO_LOAD = "outputweight_neumonia_x.h5"
     MODEL_PATH = "../../model"
 
     # Cargar la RNA desde disco
